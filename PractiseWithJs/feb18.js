0875690aa27149198c5bd16ab7ccef6843d859cb
RefayaAftarin <refayaaftarin2018@gmail.com>
2024-02-20 09:42:03 +0600

    first commit

---
+++ PractiseWithJs/feb18.js
@@ -0,0 +1,8 @@
+var quiz1=2;
+var quiz2=6;
+var avg=quiz1+quiz2;
+console.log(avg);
+var quiz1=document.getElementById("quiz1").value
+var quiz2=document.getElementById("quiz2").value
+var sum=quiz1+quiz2
+document.getElementById("avg").value=sum
